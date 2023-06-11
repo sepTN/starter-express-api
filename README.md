82228fc79fabb939dad0a1cfb064c0918312fa62
Septian <septgsk@gmail.com>
2023-06-12 01:32:23 +0700

    Changes to be committed: 	modified:   README.md 	modified:   index.js 	new file:   models/campground.js 	modified:   package-lock.json 	modified:   package.json 	new file:   views/home.ejs

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ Yo!
 
 ### Deploy it in 7 seconds: 
 
-[![Deploy to Cyclic](https://deploy.cyclic.app/button.svg)](https://deploy.cyclic.app/)
+[![Deploy to Cyclic](https://deploy.cyclic.app/button.svg)](https://app.cyclic.sh/#/join/sepTN)
 
--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const mongoose = require('mongoose');
+const db = async () => {
+    try {
+      const conn = await mongoose.connect(process.env.MONGODB_CONN_STRING);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  }
+
+const Campground = require('./models/campground');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/makecamp', async (req, res) => {
+    const camp = new Campground({ title: 'My Backyard', description: 'cheap camping!' });
+    await camp.save();
+    res.send(camp);
+});
+
+db().then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+        console.log("Server started!");
+    })
+});
